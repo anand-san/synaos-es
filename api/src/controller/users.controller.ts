77fb214893e2a093a559e1c693d82d94252a6865
Anand Sandilya <seanjin17@gmail.com>
2021-06-20 20:31:27 +0530

    Add create and update user controllers

--- api/src/controller/users.controller.ts
+++ api/src/controller/users.controller.ts
@@ -1,19 +1,30 @@
 import * as userService from "../service/users.service"
 import { Request, Response } from "express"
+import {responseMessage} from "../utils/responseUtils"
 
 export const getUsers = async (req: Request, res: Response) => {
-    let response = {
-        status: "Success",
-        message: ""
-    }
     try {
         let users = await userService.getUsers()
-        res.send(users)
+        res.send(responseMessage.message(200, users))
     } catch (e) {
-        response.status = "Error",
-            response.message = e.message || e
-    } finally {
-        res.send(response)
+        res.send(responseMessage.message(500, e.message || e))
     }
+}
 
+export const createUser = async (req: Request, res: Response) => {
+    try {
+        let users = await userService.createUser(req.body)
+        res.send(responseMessage.message(200, users))
+    } catch (e) {
+        res.send(responseMessage.message(500, e.message || e))
+    }
+}
+
+export const updateUser = async (req: Request, res: Response) => {
+    try {
+        let users = await userService.updateUser(req.body.uuid as string, req.body)
+        res.send(responseMessage.message(200, users))
+    } catch (e) {
+        res.send(responseMessage.message(500, e.message || e))
+    }
 }
