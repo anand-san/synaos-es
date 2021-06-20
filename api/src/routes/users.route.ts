19b8c2659ad25e45e2e71b59d7aaf97538e68fd2
Anand Sandilya <seanjin17@gmail.com>
2021-06-20 20:31:59 +0530

    Add createUser and updateUserRoutes

--- api/src/routes/users.route.ts
+++ api/src/routes/users.route.ts
@@ -3,4 +3,6 @@ import * as usersController from "../controller/users.controller"
 
 export const userRouter = (app: Router) => {
     app.get("/getUsers", usersController.getUsers)
+    app.post("/createUser", usersController.createUser)
+    app.post("/updateUser", usersController.updateUser)
 }
