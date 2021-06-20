e1a01f54628c64510165e5c062b47f7b40e15f95
Anand Sandilya <seanjin17@gmail.com>
2021-06-20 20:29:42 +0530

    Add Error handler middlewares

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import { userRouter } from "./routes"
+import { responseMessage } from './utils/responseUtils';
 
 dotenv.config({
   path: '.env'
@@ -10,11 +11,25 @@ dotenv.config({
   try {
     const app = express();
 
+    //Parse request body middleware
+    app.use(express.json())
+
+    //Use Routes
     userRouter(app);
 
-    ((port = process.env.APP_PORT || 5001) => {
-      app.listen(port, () => console.log(`> Listening on port ${port}`));
-    })();
+    //Handle invalid route error
+    app.get('*', function (req, res) {
+      res.status(404).send(responseMessage.message(404, "Invalid Endpoint"));
+    });
+
+    //Common error handler middleware
+    app.use(function errorHandler(err:Error, req:Request, res:Response, next:NextFunction) {
+      res.status(500)
+      res.send(responseMessage.message(500, err.message || err))
+    })
+
+
+    app.listen(process.env.APP_PORT || 5001, () => console.log(`App Running`));
   } catch (e) {
     console.log(e)
   }
