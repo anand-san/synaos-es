4c61c911d09918c31f798efcd70d31b2c7ed7014
Anand Sandilya <seanjin17@gmail.com>
2021-06-21 19:44:11 +0530

    Save user into backend on component load

--- ui/src/Components/UserCard/usercard.behaviour.ts
+++ ui/src/Components/UserCard/usercard.behaviour.ts
@@ -1,6 +1,30 @@
+import React from "react"
+import { createUser } from "../../api/request-layer"
+import { ICardProps } from "./usercard.component"
 import { useStyles } from "./usercard.styles"
 
-export const UserCardBehaviour = () => {
+export const UserCardBehaviour = (props: ICardProps) => {
     const classes = useStyles()
-    return {classes}
+
+    const [backendApiStatus, setBackendApiStatus] = React.useState<{inProgress: boolean, error: boolean}>({
+        inProgress: true,
+        error: false
+    })
+    
+    React.useEffect(() => {
+        let currentApiStatus = {
+            inProgress: false,
+            error: false
+        }
+        createUser(props.data).then(resposne => {
+            if(resposne.data.status === "Error")
+                currentApiStatus.error= true
+        }).catch(e => {
+            currentApiStatus.error= true
+        }).finally(() => {
+            setBackendApiStatus(currentApiStatus)
+        })
+    },[props.data])
+
+    return {classes, backendApiStatus}
 }
