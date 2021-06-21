b9a539658b49d96b0120fae81cd3d88078d8994c
Anand Sandilya <seanjin17@gmail.com>
2021-06-21 18:53:23 +0530

    Add usercard styles

---
+++ ui/src/Components/UserCard/usercard.styles.ts
@@ -0,0 +1,21 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export const useStyles = makeStyles({
+    paper: {
+        padding: 10,
+        margin: 10,
+        minWidth: "50vw",
+        minHeight: "40vh",
+        display: "flex",
+        flexDirection: "column",
+    },image: {
+        width: "auto",
+        borderRadius: "100%"
+    },userDetails: {
+        display: "flex",
+        justifyContent: "space-around"
+    }, dbStatus: {
+        fontWeight: "lighter",
+        fontSize: "13px"
+    }
+});
