88b6e76f047099d18021b94e3c57103afd1615d4
Anand Sandilya <seanjin17@gmail.com>
2021-06-21 18:54:28 +0530

    Create user carddesign to display api data

--- ui/src/Components/UserCard/usercard.component.tsx
+++ ui/src/Components/UserCard/usercard.component.tsx
@@ -1,10 +1,33 @@
-import React from "react";
 import { UserPropTypes } from "../../Pages/Home/home.types";
-
+import Paper from "@material-ui/core/Paper";
+import { UserCardBehaviour } from "./usercard.behaviour";
 export interface ICardProps {
-  data: UserPropTypes[];
+  data: UserPropTypes;
 }
 
 export default function UserCard(props: ICardProps) {
-  return <div>{JSON.stringify(props.data)}</div>;
+  const { classes } = UserCardBehaviour();
+  const { data: user } = props;
+  return (
+    <div>
+      <Paper className={classes.paper} elevation={5}>
+        <div>
+          <img
+            className={classes.image}
+            src={user.picture}
+            alt={`image_${user.name}`}
+          />
+          <h2>{user.name}</h2>
+        </div>
+        <div className={classes.userDetails}>
+          <p>Gender: {user.gender}</p>
+          <p>Phone: {user.phone}</p>
+          <p>Born: {new Date(user.dob as string).toDateString()}</p>
+        </div>
+        <div className={classes.dbStatus}>
+            {user.uuid} saved to db
+        </div>
+      </Paper>
+    </div>
+  );
 }
