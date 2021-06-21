f690bd960884de4ddd95be98b5ac0b8539acd4a2
Anand Sandilya <seanjin17@gmail.com>
2021-06-21 17:43:08 +0530

    Set homepage root styles

---
+++ ui/src/Pages/Home/home.styles.ts
@@ -0,0 +1,13 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export const useStyles = makeStyles({
+    root: {
+        height: "100vh",
+        width: "100vw"
+    },main: {
+        display: "flex",
+        justifyContent: "center",
+        alignContent: "center",
+        textAlign: "center"
+    }
+});
