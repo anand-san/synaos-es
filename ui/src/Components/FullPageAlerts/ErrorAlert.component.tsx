e1f0325a80a44e664b5b34685ef8c107e6a782a2
Anand Sandilya <seanjin17@gmail.com>
2021-06-22 20:48:16 +0530

    Add fullpage error alert component

---
+++ ui/src/Components/FullPageAlerts/ErrorAlert.component.tsx
@@ -0,0 +1,23 @@
+import { useStyles } from "./FullPageAlert.styles";
+import Button from "@material-ui/core/Button";
+import { useHistory } from "react-router-dom";
+
+export default function ErrorAlert() {
+  const classes = useStyles();
+  const history = useHistory();
+  return (
+    <div className={classes.root}>
+      <img src="/images/error.svg" alt="success" />
+      <p>Oops! Something went wrong</p>
+      <div className={classes.navButtons}>
+          <Button variant="outlined" onClick={() => history.push("/savedusers")}>
+        Saved Users
+      </Button>
+      <Button variant="outlined" onClick={() => history.push("/randomuser")}>
+        Random Users
+      </Button>
+      </div>
+      
+    </div>
+  );
+}
---
+++ ui/src/Components/FullPageAlerts/index.ts
@@ -0,0 +1,3 @@
+import ErrorAlert from "./ErrorAlert.component"
+
+export { ErrorAlert }
