48b24677bfc53e8a0e702231c08bc4c2d0fe615d
Anand Sandilya <seanjin17@gmail.com>
2021-06-22 22:38:42 +0530

    use UserPropTypes

--- ui/src/Pages/SavedUsers/savedusers.behaviour.ts
+++ ui/src/Pages/SavedUsers/savedusers.behaviour.ts
@@ -1,20 +1,20 @@
 import React from "react"
 import { getUsers } from "../../api/request-layer"
-
+import { UserPropTypes } from "../../Pages/RandomUser/randomuser.types";
 export default function SavedUsersBehaviour() {
 
-    const [tableData, setTableData] = React.useState([])
+    const [tableData, setTableData] = React.useState<UserPropTypes[]>([])
     const [isLoading, setIsLoading] = React.useState<boolean>(true)
     const [isError, setIsError] = React.useState<boolean>(false)
 
     React.useEffect(() => {
         getUsers().then(response => {
             let result = response.data.message
-            setTableData(result.reduce((a: Array<any>,c: any) => {
+            setTableData(result.reduce((a: UserPropTypes[],c: UserPropTypes) => {
                 a.push({
                     name: c.name,
                     gender: c.gender,
-                    dob: new Date(c.dob).toDateString(),
+                    dob: new Date(c.dob as string).toDateString(),
                     phone: c.phone,
                     picture: c.picture,
                     uuid: c.uuid,
@@ -28,5 +28,5 @@ export default function SavedUsersBehaviour() {
         })
     }, [])
 
-    return {tableData, isLoading, isError}
+    return {tableData, isLoading, isError, setTableData}
 }
