91a8d8535fc379dbe899e159c6d7397a5baec4eb
Anand Sandilya <seanjin17@gmail.com>
2021-06-22 20:47:40 +0530

    Create saved users page component

--- ui/src/Pages/SavedUsers/savedusers.component.tsx
+++ ui/src/Pages/SavedUsers/savedusers.component.tsx
@@ -1,8 +1,36 @@
+import MuiTable from "../../Components/MaterialTable";
+import SavedUsersBehaviour from "./savedusers.behaviour";
+import {ErrorAlert} from "../../Components/FullPageAlerts"
 
 export default function SavedUsers() {
-    return (
-        <div>
-            SavedUsers
-        </div>
-    )
+  const { tableData, isLoading, isError } = SavedUsersBehaviour();
+
+  if (isError) return <ErrorAlert />
+
+  return (
+    <div>
+      <MuiTable
+        columns={[
+          {
+            title: "Image",
+            field: "picture",
+            render: (rowData: any): JSX.Element => (
+              <img
+                alt={rowData.uuid}
+                src={rowData.picture}
+                style={{ width: 40, borderRadius: "50%" }}
+              />
+            ),
+          },
+          { title: "Name", field: "name" },
+          { title: "Gender", field: "gender" },
+          { title: "DOB", field: "dob" },
+          { title: "Phone", field: "phone" },
+        ]}
+        data={tableData}
+        title="Users"
+        isLoading={isLoading}
+      />
+    </div>
+  );
 }
