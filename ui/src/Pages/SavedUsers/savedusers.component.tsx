81ffddb84acc3db09785cc063e57fa7465290eb5
Anand Sandilya <seanjin17@gmail.com>
2021-06-22 22:39:29 +0530

    Add table editable actions

--- ui/src/Pages/SavedUsers/savedusers.component.tsx
+++ ui/src/Pages/SavedUsers/savedusers.component.tsx
@@ -1,20 +1,23 @@
-import MuiTable from "../../Components/MaterialTable";
+import tableIcons from "../../Components/MaterialTableIcons";
 import SavedUsersBehaviour from "./savedusers.behaviour";
-import {ErrorAlert} from "../../Components/FullPageAlerts"
+import { ErrorAlert } from "../../Components/FullPageAlerts";
+import MaterialTable from "material-table";
+import { updateUser, deleteUser } from "../../api/request-layer";
 
 export default function SavedUsers() {
-  const { tableData, isLoading, isError } = SavedUsersBehaviour();
+  const { tableData, isLoading, isError, setTableData } = SavedUsersBehaviour();
 
-  if (isError) return <ErrorAlert />
+  if (isError) return <ErrorAlert />;
 
   return (
     <div>
-      <MuiTable
+      <MaterialTable
+        icons={tableIcons}
         columns={[
           {
-            title: "Image",
+            title: "Avatar",
             field: "picture",
-            render: (rowData: any): JSX.Element => (
+            render: (rowData): JSX.Element => (
               <img
                 alt={rowData.uuid}
                 src={rowData.picture}
@@ -28,8 +31,31 @@ export default function SavedUsers() {
           { title: "Phone", field: "phone" },
         ]}
         data={tableData}
-        title="Users"
+        title="Saved Users"
         isLoading={isLoading}
+        options={{
+          // actionsColumnIndex: -1,  // Show actions buttons at end or start
+          pageSize: 5,
+          pageSizeOptions: [5, 10, 15, 20],
+          emptyRowsWhenPaging: false,
+          actionsCellStyle: { textAlign: "center" },
+        }}
+        editable={{
+          onRowUpdate: async (newData, oldData) => {
+            let newTableData: Array<any> = [...tableData];
+            const index = oldData?.tableData.id;
+            newTableData[index] = newData;
+            setTableData([...newTableData]);
+            return await updateUser(newData);
+          },
+          onRowDelete: async (oldData) => {
+            let newTableData = [...tableData];
+            const index = oldData.tableData.id;
+            newTableData.splice(index, 1);
+            setTableData(newTableData);
+            await deleteUser(oldData.uuid);
+          },
+        }}
       />
     </div>
   );
