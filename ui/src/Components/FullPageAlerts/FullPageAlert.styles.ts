4b16b56c70d968660da32d691534e8e02d4d115e
Anand Sandilya <seanjin17@gmail.com>
2021-06-22 20:49:07 +0530

    Add error component styles

---
+++ ui/src/Components/FullPageAlerts/FullPageAlert.styles.ts
@@ -0,0 +1,14 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export const useStyles = makeStyles({
+    root: {
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center"
+    },navButtons: {
+        display: "flex"
+    }
+});
