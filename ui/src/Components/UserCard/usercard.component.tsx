4ef8e5d5e3df94bb0bd3d5e87ee72e7f9484553b
Anand Sandilya <seanjin17@gmail.com>
2021-06-23 21:37:29 +0530

    Add  default values and test-ids

--- ui/src/Components/UserCard/usercard.component.tsx
+++ ui/src/Components/UserCard/usercard.component.tsx
@@ -14,15 +14,15 @@ export default function UserCard(props: ICardProps) {
         <div>
           <img
             className={classes.image}
-            src={user.picture}
-            alt={`image_${user.name}`}
+            src={user.picture || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScdGAFZS8P9rXmHkXMDp_vgYHzKMsrO5xSww&usqp=CAU"}
+            alt={`image_${user.phone}`}
           />
-          <h2>{user.name}</h2>
+          <h2 data-testid="user_name">{user.name || "NA"}</h2>
         </div>
         <div className={classes.userDetails}>
-          <p>Gender: {user.gender}</p>
-          <p>Phone: {user.phone}</p>
-          <p>Born: {new Date(user.dob as string).toDateString()}</p>
+          <p data-testid="user_gender">Gender: {user.gender || "NA"}</p>
+          <p data-testid="user_phone">Phone: {user.phone}</p>
+          <p data-testid="user_born">Born: {user.dob ? new Date(user.dob as string).toDateString() : "NA"}</p>
         </div>
         <div className={classes.dbStatus} style={{color: backendApiStatus.error ? "red" : "green"}}>
             {backendApiStatus.error ? "Failed to save" : backendApiStatus.inProgress ? "Saving" : "Saved"}
